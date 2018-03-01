c8e01bb9274f00186cd206954e7cece2cff51173
glaera <31164368+glaera@users.noreply.github.com>
2018-03-01 20:47:12 +0100

    Update init.js

--- init.js
+++ init.js
@@ -2,9 +2,11 @@ load('api_mqtt.js');
 load('api_gpio.js');
 load('api_timer.js');
 load('api_dht.js');
+load('api_sys.js');
 
-let pin = 0, topic = 'light';
-
+let pin = 0, topicLight = 'glaera' + '/feeds/light/json';
+let statusTrigger =  'glaera'  + '/feeds/status/json';
+let stateTopic = 'glaera' + '/feeds/state/json'; 
 let LED = 12;
 let tpin = 14;
 
@@ -17,58 +19,45 @@ GPIO.write(LED, true)
 
 GPIO.set_button_handler(pin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function() {
   print('going to send');
-  let t = dht.getTemp();
-  let h = dht.getHumidity();
-
-  if (isNaN(h) || isNaN(t)) {
-    print('Failed to read data from sensor');
-   
-  }
-
-  print('Temperature:', t, '*C');
-  print('Humidity:', h, '%');
-  MQTT.pub('light', JSON.stringify(t));
-  print('sent');
+  
+  let ok = MQTT.pub( stateTopic, getState());
+  print('status sent?',ok);
+ 
 }, null);
 
-MQTT.sub(topic, function(conn, topic, msg) {
-  print('Topic:', topic, 'message:', msg);
-  if (msg === 'ON' || msg === '1') {
+MQTT.sub(topicLight, function(conn, topic, msg) {
+  print('Teceived Topic:', topic, 'message:', msg);
+  let jmsg = JSON.parse(msg);
+  if (jmsg.last_value === 'ON' || jmsg.last_value === '1') {
     GPIO.write(LED, true);
   } else {
     GPIO.write(LED, false);
+    print('turning off')
   }
 }, null);
 
-MQTT.sub("status", function(conn, topic, msg) {
-  print('Topic:', topic, 'message:', msg);
+MQTT.sub(statusTrigger, function(conn, topic, msg) {
+  print('Received Topic:', topic, 'message:', msg);
  
-  let t = dht.getTemp();
-  let h = dht.getHumidity();
 
-  if (isNaN(h) || isNaN(t)) {
-    print('Failed to read data from sensor');
-   
-  }
-  
   let LED_STATE = GPIO.read(LED);
 
-  print('Temperature:', t, '*C');
-  print('Humidity:', h, '%');
-  MQTT.pub('state', getState());
+  MQTT.pub( stateTopic, getState());
  
 }, null);
 
 function getState() {
-  let t = dht.getTemp();
-  let h = dht.getHumidity();
+  let t = JSON.stringify(dht.getTemp());
+  let h = JSON.stringify(dht.getHumidity());
 
   if (isNaN(h) || isNaN(t)) {
     print('Failed to read data from sensor');
    
   }
   
-  return JSON.stringify({temperature:t,light:LED_STATE,humidity:h});
-  
-}
+  let LED_STATE = JSON.stringify(GPIO.read(LED));
 
+  //let toreturn = JSON.stringify({temperature:t,light:LED_STATE,humidity:h});
+  let toreturn = t + ',' + h + ',' +LED_STATE;
+  return toreturn;
+}
